d32c75821f3ef843921215a9c8d70abf03eb3c7e
aphshir <45636296+aphshir@users.noreply.github.com>
2023-04-09 17:06:01 +0200

    Small update
    deleted commented code and unused variables

--- morpion.py
+++ morpion.py
@@ -16,8 +16,6 @@ class morpion(tk.Frame):
         --------
         A3|B3|C3
         """
-        self.score_cross = 0
-        self.score_circle = 0
         tk.Frame.__init__(self, *args, **kwargs)
         self.c = args
         turtle.TurtleScreen._RUNNING = True
@@ -237,42 +235,6 @@ class morpion(tk.Frame):
         B3.grid(row=3, column=1)
         C3 = ttk.Button(t, text="C3",state=self.status[8], command=lambda: submit("C3", actual_player))
         C3.grid(row=3, column=2)
-"""
-def round():
-    game=morpion()
-    t = tk.Toplevel()
-    t.geometry("500x500")
-    t.minsize(500, 500)
-    t.maxsize(500, 500)
-    def quit():
-        sys.exit()
-    t.resizable(False, False)
-    t.protocol("WM_DELETE_WINDOW", lambda: quit())
-    actual_player="x"
-    while True:
-        lab = "tour de "+actual_player
-        tk.Label(t,text=lab).grid(row=0,column=1)
-        A1= ttk.Button(t,text="A1",command=lambda:game.add("A1",actual_player))
-        A1.grid(row=1,column=0)
-        B1 = ttk.Button(t, text="B1", command=lambda: game.add("B1", actual_player))
-        B1.grid(row=1, column=1)
-        C1 = ttk.Button(t, text="C1", command=lambda:game.add("C1",actual_player))
-        C1.grid(row=1,column=2)
-        A2 = ttk.Button(t,text="A2", command=lambda:game.add("A2",actual_player))
-        A2.grid(row=2,column=0)
-        B2= ttk.Button(t,text="B2", command=lambda: game.add("B2",actual_player))
-        B2.grid(row=2,column=1)
-        C2 = ttk.Button(t,text="C2", command=lambda: game.add("C2", actual_player))
-        C2.grid(row=2,column=2)
-        A3 = ttk.Button(t,text="A3",command=lambda: game.add("A3",actual_player))
-        A3.grid(row=3,column=0)
-        B3 = ttk.Button(t,text="B3",command=lambda:game.add("B3",actual_player))
-        B3.grid(row=3,column=1)
-        C3 = ttk.Button(t,text="C3",command=lambda:game.add("C3",actual_player))
-        C3.grid(row=3,column=2)
-
-round()
-"""
 def start():
     root = tk.Tk()
     win = root
